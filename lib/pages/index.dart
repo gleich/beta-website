ddd77cca83f1a523b1477beb2ecd7daae01a0279
Matthew Gleich <matthewgleich@gmail.com>
2020-01-03 16:08:30 -0500

    Update application

---
+++ lib/pages/index.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "Hello! This site is currently under construction.",
+        style: TextStyle(
+          fontSize: 25,
+          color: Colors.white
+        ),
+      ),
+    );
+  }
+}
