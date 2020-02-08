05a91539c2e250e44d24407a9e2ac0043da381bb
Matthew Gleich <matthewgleich@gmail.com>
2020-02-08 18:33:04 -0500

    Add Twitter social icon

--- lib/pages/index.dart
+++ lib/pages/index.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_linkify/flutter_linkify.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,6 +10,9 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Matthew Gleich"),
         backgroundColor: Colors.black,
+        actions: <Widget>[
+          SocialMediaIcon(MdiIcons.twitter, "https://twitter.com/GleichMatthew")
+        ],
       ),
       body: Center(
         child: Row(
@@ -54,6 +58,30 @@ class HomePage extends StatelessWidget {
         ),
       ),
       backgroundColor: Color.fromRGBO(63, 63, 63, 1.0),
+    );
+  }
+}
+
+class SocialMediaIcon extends StatelessWidget {
+  final String linkURL;
+  final icon;
+
+  SocialMediaIcon(this.icon, this.linkURL);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        icon,
+        color: Colors.white,
+      ),
+      onPressed: () async {
+        if (await canLaunch(linkURL)) {
+          await launch(linkURL);
+        } else {
+          throw "Could not launch $linkURL";
+        }
+      },
     );
   }
 }
