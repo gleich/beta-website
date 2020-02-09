2503a379450a9702512ab21e16c1023c49b87df4
Matthew Gleich <matthewgleich@gmail.com>
2020-02-08 22:12:08 -0500

    Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,11 +1,13 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/index.html": "d0c3b4837ff1920387bcbb445f72afc5",
-"/main.dart.js": "2a82a3c47d89e046fea570526a459048",
-"/assets/LICENSE": "b94fd3ea81edcd551763dc7ef3d87653",
-"/assets/AssetManifest.json": "f991983e4b74ce3d25e0a253740ca595",
-"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+  "/index.html": "1c814cad5b5964071816f4a74fcfd028",
+"/main.dart.js": "feeeb565a8cb9a29deab4d18aed8fd29",
+"/favicon.png": "e94e2a82fde16d2ac04c1ac95f42ce9c",
+"/assets/LICENSE": "eb1416ec4a382f89722b804710d953ab",
+"/assets/AssetManifest.json": "5ff5dc8643a27a9e3c4354d98fad7028",
+"/assets/FontManifest.json": "0621fb7723859a382fc19210904f6578",
+"/assets/packages/material_design_icons_flutter/lib/fonts/materialdesignicons-webfont.ttf": "2abd86f90b3fd9d18d950195282cd8d5",
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "/assets/assets/images/logo.png": "e94e2a82fde16d2ac04c1ac95f42ce9c"
@@ -30,9 +32,7 @@ self.addEventListener('fetch', function (event) {
         if (response) {
           return response;
         }
-        return fetch(event.request, {
-          credentials: 'include'
-        });
+        return fetch(event.request);
       })
   );
 });
