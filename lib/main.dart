eb55c19b6601d4a00471d2d48b00f153ad9ea96d
Matthew Gleich <matthewgleich@gmail.com>
2020-02-16 11:31:12 -0500

    Merge MyApp and MySite widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import './pages/index/index.dart';
 
-void main() => runApp(_MyApp());
+void main() => runApp(_MySite());
 
-class _MyApp extends StatelessWidget {
+class _MySite extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -13,14 +12,7 @@ class _MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "Matthew Gleich",
       theme: ThemeData.dark(),
-      home: _MySite(),
+      home: HomePage(),
     );
-  }
-}
-
-class _MySite extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return HomePage();
   }
 }
