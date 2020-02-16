186be96eb062ed6b5378f6bcc2c617c6c8ed7d32
Matthew Gleich <matthewgleich@gmail.com>
2020-02-16 12:43:57 -0500

    Add openSource page

---
+++ lib/pages/openSource/openSource.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import './../../widgets/pageTemplate.dart';
+import './../../widgets/secondaryPageLeadingButton.dart';
+
+class OpenSource extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageTemplate(
+      endText: "- Open Source",
+      leadingButton: SecondaryPageLeadingButton(),
+      body: Text(""),
+    );
+  }
+}
