41f53c795e61ae30b9bf9f59d372ea2a941e34cd
Matthew Gleich <matthewgleich@gmail.com>
2020-02-22 10:41:04 -0500

    Remove template comment

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import './pages/index/index.dart';
 void main() => runApp(_MySite());
 
 class _MySite extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
