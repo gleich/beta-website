bf989e817f3c1e2baf19f4975fc9824be5030b67
Matthew Gleich <matthewgleich@gmail.com>
2020-03-14 14:50:28 -0400

    Remove animations from page buttons

--- lib/pages/index/widgets/horizontalView.dart
+++ lib/pages/index/widgets/horizontalView.dart
@@ -8,7 +8,6 @@ import './../../aboutMe/aboutMe.dart';
 import './../../photography/photography.dart';
 import './../../openSource/openSource.dart';
 import './../../cycling/cycling.dart';
-import './../../../widgets/animations.dart';
 
 class HorizontalView extends StatelessWidget {
   @override
@@ -20,44 +19,26 @@ class HorizontalView extends StatelessWidget {
         Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FadeIn(
-              delay: 3,
-              child: PageButton(
-                route: OpenSource(),
-                pageName: "Open Source",
-                leftPadding: 0,
-                topPadding: 0,
-                rightPadding: 0,
-              ),
-              start: -130,
-              stop: 0,
-              duration: const Duration(milliseconds: 700),
+            PageButton(
+              route: OpenSource(),
+              pageName: "Open Source",
+              leftPadding: 0,
+              topPadding: 0,
+              rightPadding: 0,
             ),
-            FadeIn(
-              delay: 3.3,
-              child: PageButton(
-                route: Photography(),
-                pageName: "Photography",
-                leftPadding: 0,
-                topPadding: 30,
-                rightPadding: 100,
-              ),
-              start: -130,
-              stop: 0,
-              duration: const Duration(milliseconds: 700),
+            PageButton(
+              route: Photography(),
+              pageName: "Photography",
+              leftPadding: 0,
+              topPadding: 30,
+              rightPadding: 100,
             ),
-            FadeIn(
-              delay: 3.6,
-              child: PageButton(
-                route: Cycling(),
-                pageName: "Cycling",
-                leftPadding: 0,
-                topPadding: 30,
-                rightPadding: 0,
-              ),
-              start: -130,
-              stop: 0,
-              duration: const Duration(milliseconds: 700),
+            PageButton(
+              route: Cycling(),
+              pageName: "Cycling",
+              leftPadding: 0,
+              topPadding: 30,
+              rightPadding: 0,
             )
           ],
         ),
@@ -65,44 +46,26 @@ class HorizontalView extends StatelessWidget {
         Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FadeIn(
-              delay: 3,
-              start: 130,
-              stop: 0,
-              duration: const Duration(milliseconds: 700),
-              child: PageButton(
-                route: AboutMe(),
-                pageName: "About Me",
-                leftPadding: 0,
-                topPadding: 0,
-                rightPadding: 0,
-              ),
+            PageButton(
+              route: AboutMe(),
+              pageName: "About Me",
+              leftPadding: 0,
+              topPadding: 0,
+              rightPadding: 0,
             ),
-            FadeIn(
-              delay: 3.3,
-              start: 130,
-              stop: 0,
-              duration: const Duration(milliseconds: 700),
-              child: PageButton(
-                route: Projects(),
-                pageName: "Projects",
-                leftPadding: 100,
-                topPadding: 30,
-                rightPadding: 0,
-              ),
+            PageButton(
+              route: Projects(),
+              pageName: "Projects",
+              leftPadding: 100,
+              topPadding: 30,
+              rightPadding: 0,
             ),
-            FadeIn(
-              delay: 3.6,
-              start: 130,
-              stop: 0,
-              duration: const Duration(milliseconds: 700),
-              child: PageButton(
-                route: Languages(),
-                pageName: "Languages",
-                leftPadding: 0,
-                topPadding: 30,
-                rightPadding: 0,
-              ),
+            PageButton(
+              route: Languages(),
+              pageName: "Languages",
+              leftPadding: 0,
+              topPadding: 30,
+              rightPadding: 0,
             ),
           ],
         )
--- lib/pages/index/widgets/verticalView.dart
+++ lib/pages/index/widgets/verticalView.dart
@@ -8,7 +8,6 @@ import './../../aboutMe/aboutMe.dart';
 import './../../photography/photography.dart';
 import './../../openSource/openSource.dart';
 import './../../cycling/cycling.dart';
-import './../../../widgets/animations.dart';
 
 class VerticalView extends StatelessWidget {
   @override
@@ -19,83 +18,47 @@ class VerticalView extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           const Logo(),
-          FadeIn(
-            delay: 3,
-            start: -130,
-            stop: 0,
-            duration: const Duration(milliseconds: 700),
-            child: PageButton(
-              route: OpenSource(),
-              pageName: "Open Source",
-              leftPadding: 0,
-              topPadding: 10,
-              rightPadding: 0,
-            ),
+          PageButton(
+            route: OpenSource(),
+            pageName: "Open Source",
+            leftPadding: 0,
+            topPadding: 10,
+            rightPadding: 0,
           ),
-          FadeIn(
-            delay: 3,
-            start: 130,
-            stop: 0,
-            duration: const Duration(milliseconds: 700),
-            child: PageButton(
-              route: Photography(),
-              pageName: "Photography",
-              leftPadding: 0,
-              topPadding: 10,
-              rightPadding: 0,
-            ),
+          PageButton(
+            route: Photography(),
+            pageName: "Photography",
+            leftPadding: 0,
+            topPadding: 10,
+            rightPadding: 0,
           ),
-          FadeIn(
-            delay: 3,
-            start: -130,
-            stop: 0,
-            duration: const Duration(milliseconds: 700),
-            child: PageButton(
-              route: Cycling(),
-              pageName: "Cycling",
-              leftPadding: 0,
-              topPadding: 10,
-              rightPadding: 0,
-            ),
+          PageButton(
+            route: Cycling(),
+            pageName: "Cycling",
+            leftPadding: 0,
+            topPadding: 10,
+            rightPadding: 0,
           ),
-          FadeIn(
-            delay: 3,
-            start: 130,
-            stop: 0,
-            duration: const Duration(milliseconds: 700),
-            child: PageButton(
-              route: AboutMe(),
-              pageName: "About Me",
-              leftPadding: 0,
-              topPadding: 10,
-              rightPadding: 0,
-            ),
+          PageButton(
+            route: AboutMe(),
+            pageName: "About Me",
+            leftPadding: 0,
+            topPadding: 10,
+            rightPadding: 0,
           ),
-          FadeIn(
-            delay: 3,
-            start: -130,
-            stop: 0,
-            duration: const Duration(milliseconds: 700),
-            child: PageButton(
-              route: Projects(),
-              pageName: "Projects",
-              leftPadding: 0,
-              topPadding: 10,
-              rightPadding: 0,
-            ),
+          PageButton(
+            route: Projects(),
+            pageName: "Projects",
+            leftPadding: 0,
+            topPadding: 10,
+            rightPadding: 0,
           ),
-          FadeIn(
-            delay: 3,
-            start: 130,
-            stop: 0,
-            duration: const Duration(milliseconds: 700),
-            child: PageButton(
-              route: Languages(),
-              pageName: "Languages",
-              leftPadding: 0,
-              topPadding: 10,
-              rightPadding: 0,
-            ),
+          PageButton(
+            route: Languages(),
+            pageName: "Languages",
+            leftPadding: 0,
+            topPadding: 10,
+            rightPadding: 0,
           )
         ],
       ),
