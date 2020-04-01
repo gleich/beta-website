90ece1af9807cf01bef34d64f2a9847d0c330d63
Matthew Gleich <matthewgleich@gmail.com>
2020-04-01 01:30:34 -0400

    🎨 Refactor page not found page

--- lib/pages/404/404.dart
+++ lib/pages/404/404.dart
@@ -6,11 +6,16 @@ class Page404 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
+            Text(
+              "404 Error",
+              style: TextStyle(fontSize: 70),
+            ),
             Text(
               "Page not found 😔",
               style: TextStyle(fontSize: 70),
@@ -19,10 +24,13 @@ class Page404 extends StatelessWidget {
             RaisedButton(
               onPressed: () =>
                   Navigator.popAndPushNamed(context, IndexPage.pageName),
+              color: Theme.of(context).accentColor,
+              hoverElevation: 10,
               child: Text(
                 "Back To Home",
                 style: TextStyle(
                   fontSize: 50,
+                  color: Theme.of(context).primaryColor,
                 ),
               ),
             ),
