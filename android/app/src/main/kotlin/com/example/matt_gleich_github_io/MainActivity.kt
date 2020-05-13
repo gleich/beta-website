3b044be22e2a34dd46660d3bcaa32920d6307bb1
Matthew Gleich <matthewgleich@gmail.com>
2020-05-13 13:02:43 -0400

    🍎 Remove no longer supported platforms

--- android/app/src/main/kotlin/com/example/matt_gleich_github_io/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.matt_gleich_github_io
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/gleich/personal_site/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.gleich.personal_site
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
