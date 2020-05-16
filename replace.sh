4301c66734c73d9a477f0a509970961fe7c81b69
Caleb Hagner <caleb.hagner@pm.me>
2020-05-16 06:40:48 -0400

    Update each build with manifest.json

--- replace.sh
+++ replace.sh
@@ -7,6 +7,7 @@ rm -rf assets
 rm -rf flutter_service_worker.js
 rm -rf main.dart.js.map
 rm -rf index.html
+rm -rf manifest.json
 
 # Copy files from build
 cp -r build/web/assets .
@@ -15,5 +16,6 @@ cp build/web/flutter_service_worker.js .
 cp build/web/index.html .
 cp build/web/main.dart.js .
 cp build/web/main.dart.js.map .
+cp build/web/manifest.json
 
 echo "Done"
