a21f557d47ea21be8d34aad6ed8e45f7e3b43d5e
Matthew Gleich <matthewgleich@gmail.com>
2020-06-13 07:39:59 -0400

    🔥 Remove markdown features
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- lib/routes/blog/blog.dart
+++ lib/routes/blog/blog.dart
@@ -1,43 +1,14 @@
-// 🎯 Dart imports:
-import 'dart:convert';
-import 'dart:io';
-
 // 🐦 Flutter imports:
 import 'package:flutter/material.dart';
 
 // 🌎 Project imports:
-import 'package:personal_site/widgets/pageTemplate.dart';
+import 'package:personal_site/widgets/workingOn.dart';
 
 class BlogRoute extends StatelessWidget {
-  static const routeName = '/blog';
-
-  Future<Map<String, String>> _articles(BuildContext context) async {
-    final manifestContent =
-        await DefaultAssetBundle.of(context).loadString('AssetManifest.json');
-    final Map<String, dynamic> manifestMap = json.decode(manifestContent);
-    final contents = <String>[];
-    for (final file in manifestMap.keys
-        .where((article) => article.startsWith("assets/markdown/"))
-        .toList()) {
-      contents.add(await DefaultAssetBundle.of(context).loadString(file));
-    }
-    return contents;
-  }
+  static const routeName = "/blog";
 
   @override
   Widget build(BuildContext context) {
-    return PageTemplate(
-      body: FutureBuilder<Map<String, String>>(
-        future: _articles(context),
-        builder: (BuildContext context,
-                AsyncSnapshot<Map<String, String>> snapshot) =>
-            snapshot.connectionState == ConnectionState.done
-                ? Text(
-                    snapshot.data.toString(),
-                  )
-                : CircularProgressIndicator(),
-      ),
-      pageName: '🖊️ Blog',
-    );
+    return WorkingOn();
   }
 }
